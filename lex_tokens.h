3bf153739241b86bba0872dde16dbfa1de8db5eb
Khavar Facey <108436274+khavarpython@users.noreply.github.com>
2026-03-12 00:30:59 -0500

    Update lex_tokens.h

--- lex_tokens.h
+++ lex_tokens.h
@@ -21,46 +21,48 @@
 #define DISPLAY 18
 #define INPUT 19
 #define READ 20
-#define BOOL_LIT 21
-#define NULL_LIT 22
-#define DOUBLE_LIT 23
-#define FLOAT_LIT 24
-#define INT_LIT 25
-#define STRING_LIT 26
-#define CHAR_LIT 27
-#define ADD 28
-#define SUB 29
-#define MUL 30
-#define DIV 31
-#define MOD 32
-#define POW 33
-#define ASSIGN 34
-#define ASSIGN_ADD 35
-#define ASSIGN_SUB 36
-#define ASSIGN_MUL 37
-#define ASSIGN_DIV 38
-#define INCREM 39
-#define DECREM 40
-#define EQUAL 41
-#define NEQUAL 42
-#define LESS_THAN 43
-#define LESS_THAN_E 44
-#define GREATER_THAN 45
-#define GREATER_THAN_E 46
-#define AND 47
-#define OR 48
-#define NOT 49
-#define LPAREN 50
-#define RPAREN 51
-#define LBRACE 52
-#define RBRACE 53
-#define LBRACKET 54
-#define RBRACKET 55
-#define SEMICOLON 56
-#define COLON 57
-#define COMMA 58
-#define DOT 59
-#define IDENTIFIER 60
+#define LET 21
+#define CONST 22
+#define BOOL_LIT 23
+#define NULL_LIT 24
+#define DOUBLE_LIT 25
+#define FLOAT_LIT 26
+#define INT_LIT 27
+#define STRING_LIT 28
+#define CHAR_LIT 29
+#define ADD 30
+#define SUB 31
+#define MUL 32
+#define DIV 33
+#define MOD 34
+#define POW 35
+#define ASSIGN 36
+#define ASSIGN_ADD 37
+#define ASSIGN_SUB 38
+#define ASSIGN_MUL 39
+#define ASSIGN_DIV 40
+#define INCREM 41
+#define DECREM 42
+#define EQUAL 43
+#define NEQUAL 44
+#define LESS_THAN 45
+#define LESS_THAN_E 46
+#define GREATER_THAN 47
+#define GREATER_THAN_E 48
+#define AND 49
+#define OR 50
+#define NOT 51
+#define LPAREN 52
+#define RPAREN 53
+#define LBRACE 54
+#define RBRACE 55
+#define LBRACKET 56
+#define RBRACKET 57
+#define SEMICOLON 58
+#define COLON 59
+#define COMMA 60
+#define DOT 61
+#define IDENTIFIER 62
 
 typedef union
 {
@@ -69,8 +71,5 @@ typedef union
     double dval;
     char *sval;
 } YYSTYPE;
-
 YYSTYPE yylval;
-
-
 #endif
