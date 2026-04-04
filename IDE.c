7f0ae370115dd5e8aa605d57bfaf85f497ace522
Khavar Facey <khavarfaceyu@gmail.com>
2026-04-04 14:10:13 -0500

    Gets output from the generated c file

--- IDE.c
+++ IDE.c
@@ -5,7 +5,7 @@
 GtkTextBuffer *source_buf;
 GtkTextBuffer *output_buf;
 
-/* Append text to output */
+// Append text to output
 void append_output(const char *text)
 {
     GtkTextIter end;
@@ -13,52 +13,50 @@ void append_output(const char *text)
     gtk_text_buffer_insert(output_buf, &end, text, -1);
 }
 
-/* Run compiler using run.sh */
+// Run compiler using run.sh
 void run_code(GtkButton *btn, gpointer data)
 {
-
     GtkTextIter start, end;
     gtk_text_buffer_get_bounds(source_buf, &start, &end);
-
     gchar *code = gtk_text_buffer_get_text(source_buf, &start, &end, FALSE);
 
-    /* Save code to file */
+    // Save code to file
     FILE *f = fopen("input.apl", "w");
     if (!f)
     {
         append_output("Error: Cannot write file\n");
+        g_free(code);
         return;
     }
-
     fputs(code, f);
     fclose(f);
     g_free(code);
 
-    /* Clear output */
+    // Clear output
     gtk_text_buffer_set_text(output_buf, "", -1);
-
     append_output("Running...\n\n");
 
-    /* Run script */
-    FILE *pipe = popen("./run.sh < input.apl", "r");
-
+    // Run script
+    FILE *pipe = popen("bash run.sh input.apl 2>&1", "r");
     if (!pipe)
     {
-        append_output("Error running script\n");
+        append_output("Error: Could not run script\n");
         return;
     }
 
     char buffer[512];
-
     while (fgets(buffer, sizeof(buffer), pipe))
     {
         append_output(buffer);
+        // Force GTK to redraw so output appears line by line
+        while (gtk_events_pending())
+            gtk_main_iteration();
     }
-
     pclose(pipe);
+    append_output("\nDone.\n");
 }
 
-/* Build GUI */
+// Build GUI
 void activate(GtkApplication *app, gpointer user_data)
 {
 
@@ -69,7 +67,7 @@ void activate(GtkApplication *app, gpointer user_data)
     GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
     gtk_container_add(GTK_CONTAINER(window), vbox);
 
-    /* Editor */
+    // Editor
     GtkWidget *editor = gtk_text_view_new();
     source_buf = gtk_text_view_get_buffer(GTK_TEXT_VIEW(editor));
 
@@ -77,12 +75,12 @@ void activate(GtkApplication *app, gpointer user_data)
     gtk_container_add(GTK_CONTAINER(scroll1), editor);
     gtk_box_pack_start(GTK_BOX(vbox), scroll1, TRUE, TRUE, 0);
 
-    /* Run button */
+    // Run button
     GtkWidget *run_btn = gtk_button_new_with_label("Run");
     g_signal_connect(run_btn, "clicked", G_CALLBACK(run_code), NULL);
     gtk_box_pack_start(GTK_BOX(vbox), run_btn, FALSE, FALSE, 5);
 
-    /* Output */
+    // Output
     GtkWidget *output = gtk_text_view_new();
     output_buf = gtk_text_view_get_buffer(GTK_TEXT_VIEW(output));
     gtk_text_view_set_editable(GTK_TEXT_VIEW(output), FALSE);
@@ -94,7 +92,7 @@ void activate(GtkApplication *app, gpointer user_data)
     gtk_widget_show_all(window);
 }
 
-/* Main */
+// Main
 int main(int argc, char **argv)
 {
 
